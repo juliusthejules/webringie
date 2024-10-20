9b2983d24e5597056753e408e53ed558b8ca6bc0
Joseph D. Smith <142750447+juliusthejules@users.noreply.github.com>
2024-10-19 23:55:06 -0500

    Update webringie.js

--- js/webringie.js
+++ js/webringie.js
@@ -1,7 +1,8 @@
 fetch('./API/metadata.json')
     .then(response => response.json())
     .then(data => {
-        // Inject keywords into the head section
+        // Inject metadata into the head section
+
         if (data.keywords) {
             let metaKeywords = document.createElement('meta');
             metaKeywords.name = 'keywords';
@@ -9,7 +10,6 @@ fetch('./API/metadata.json')
             document.head.appendChild(metaKeywords);
         }
 
-        // Inject viewport settings
         if (data.viewport) {
             let metaViewport = document.createElement('meta');
             metaViewport.name = 'viewport';
@@ -17,7 +17,6 @@ fetch('./API/metadata.json')
             document.head.appendChild(metaViewport);
         }
 
-        // Inject robots settings
         if (data.robots) {
             let metaRobots = document.createElement('meta');
             metaRobots.name = 'robots';
@@ -25,15 +24,13 @@ fetch('./API/metadata.json')
             document.head.appendChild(metaRobots);
         }
 
-        // Inject content-language
-        if (data['Content-Language']) {
+        if (data["Content-Language"]) {
             let metaLanguage = document.createElement('meta');
             metaLanguage.httpEquiv = 'Content-Language';
-            metaLanguage.content = data['Content-Language'];
+            metaLanguage.content = data["Content-Language"];
             document.head.appendChild(metaLanguage);
         }
 
-        // Inject distribution settings
         if (data.distribution) {
             let metaDistribution = document.createElement('meta');
             metaDistribution.name = 'distribution';
@@ -41,12 +38,24 @@ fetch('./API/metadata.json')
             document.head.appendChild(metaDistribution);
         }
 
-        // Inject charset encoding
-        if (data.charset) {
-    let metaCharset = document.createElement('meta');
-    metaCharset.setAttribute('charset', data.charset);
-    document.head.appendChild(metaCharset);
+        if (data["cache-control"]) {
+            let metaCacheControl = document.createElement('meta');
+            metaCacheControl.httpEquiv = 'cache-control';
+            metaCacheControl.content = data["cache-control"];
+            document.head.appendChild(metaCacheControl);
         }
-        
+
+        if (data.pragma) {
+            let metaPragma = document.createElement('meta');
+            metaPragma.httpEquiv = 'pragma';
+            metaPragma.content = data.pragma;
+            document.head.appendChild(metaPragma);
+        }
+
+        if (data.charset) {
+            let metaCharset = document.createElement('meta');
+            metaCharset.setAttribute('charset', data.charset);
+            document.head.appendChild(metaCharset);
+        }
     })
     .catch(error => console.error('Error loading metadata:', error));
